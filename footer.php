761d9c302b894e3048a4fb654ea44b94e16d394d
Kevan Ngan <kevanngan_@hotmail.com>
2024-02-16 12:43:57 -0800

    Init

--- footer.php
+++ footer.php
@@ -16,7 +16,9 @@
 			
 		</div><!-- .footer-contact -->
 		<div class="footer-menus">
-				
+			<nav id="footer-navigation" class="footer-navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-left') ); ?>
+			</nav>
 		</div><!-- .footer-menus -->
 		<div class="site-info">
 			<?php esc_html_e( 'Created by ', 'fwd' ); ?><a href="<?php echo esc_url( __( 'https://wp.bcitwebdeveloper.ca/', 'fwd' ) ); ?>"><?php esc_html_e( 'Jonathon Leathers', 'fwd' ); ?></a>
--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ function fwd_setup() {
 	register_nav_menus(
 		array(
 			'header' => esc_html__( 'Header Menu Location', 'fwd' ),
+			'footer-left' => esc_html__( 'Footer Menu Location', 'fwd' ),
 		)
 	);
 
