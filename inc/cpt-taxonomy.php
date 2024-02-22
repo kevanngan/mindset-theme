c04b15e551d54d65d22f7d3ba83ad220dcb40ae9
Kevan Ngan <kevanngan_@hotmail.com>
2024-02-22 12:32:27 -0800

    Add testimonials cpt

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -50,6 +50,45 @@ function fwd_register_custom_post_types() {
 
     register_post_type( 'fwd-work', $args );
 
+    // Register Testimonials CPT
+    $labels = array(
+        'name'               => _x( 'Testimonials', 'post type general name'  ),
+        'singular_name'      => _x( 'Testimonial', 'post type singular name'  ),
+        'menu_name'          => _x( 'Testimonials', 'admin menu'  ),
+        'name_admin_bar'     => _x( 'Testimonial', 'add new on admin bar' ),
+        'add_new'            => _x( 'Add New', 'testimonial' ),
+        'add_new_item'       => __( 'Add New Testimonial' ),
+        'new_item'           => __( 'New Testimonial' ),
+        'edit_item'          => __( 'Edit Testimonial' ),
+        'view_item'          => __( 'View Testimonial'  ),
+        'all_items'          => __( 'All Testimonials' ),
+        'search_items'       => __( 'Search Testimonials' ),
+        'parent_item_colon'  => __( 'Parent Testimonials:' ),
+        'not_found'          => __( 'No testimonials found.' ),
+        'not_found_in_trash' => __( 'No testimonials found in Trash.' ),
+    );
+    
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'testimonials' ),
+        'capability_type'    => 'post',
+        'has_archive'        => false,
+        'hierarchical'       => false,
+        'menu_position'      => 7,
+        'menu_icon'          => 'dashicons-heart',
+        'supports'           => array( 'title', 'editor' ),
+        'template'           => array( array( 'core/pullquote' ) ),
+        'template_lock'      => 'all'
+    );
+    
+    register_post_type( 'fwd-testimonial', $args );
+
 }
 add_action( 'init', 'fwd_register_custom_post_types' );
 
--- page-about.php
+++ page-about.php
@@ -31,6 +31,21 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
+		<?php 
+		$args = array(
+			'post_type' => 'fwd-testimonial',
+			'posts_per_page' => -1,
+		);
+		$query = new WP_Query( $args );
+		if ( $query -> have_posts() ) {
+			while ( $query -> have_posts() ) {
+				$query -> the_post();
+				the_content();
+			}
+			wp_reset_postdata();
+		}
+		?>
+		
 	</main><!-- #primary -->
 
 <?php
